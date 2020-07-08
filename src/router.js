0eee828989969f2f890443cd6d0c9e85f822a3fe
weili <weiwork0012010@sina.com>
2020-07-08 23:23:34 +0800

    路由封装

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './components/home'
-import Index from './components/index'
-import Product from './components/product'
-import Detail from './components/detail'
-import Cart from './components/cart'
+import Home from './pages/home'
+import Index from './pages/index'
+import Product from './pages/product'
+import Detail from './pages/detail'
+import Cart from './pages/cart'
+import Order from './pages/order'
+import OrderConfirm from './pages/orderConfirm'
+import OrderList from './pages/orderList'
+import OrderPay from './pages/orderPay'
+import AliPay from './pages/alipay'
 Vue.use(Router);
 export default new Router({
   routes:[
@@ -12,6 +17,7 @@ export default new Router({
          path:'/',
          name:'home',
          component:Home,
+         redirect:'/index',
          children:[
              {
                path:'/index',
@@ -27,10 +33,38 @@ export default new Router({
                 component:Detail,  
               }
          ]
-      },{
+      },
+      {
         path:'/cart',
         name:'cart',
         component:Cart,  
+      },
+      {
+        path:'/order',
+        name:'order',
+        component:Order, 
+        children:[
+            {
+             path:'list',
+             name:'order-list',
+             component:OrderList,
+            },
+            {
+                path:'confirm',
+                name:'order-confirm',
+                component:OrderConfirm,
+            },
+            {
+                path:'pay',
+                name:'order-pay',
+                component:OrderPay,
+            },
+            {
+                path:'alipay',
+                name:'alipay',
+                component:AliPay,
+            }
+        ] 
       }
   ]
 });
