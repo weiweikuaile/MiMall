045177c05770bdde7001e175c668af05c3dfaa05
weili <weiwork0012010@sina.com>
2020-07-11 19:29:28 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+//根据前端的跨域方式做调整 暂定是接口代理方式
+axios.defaults.baseURL='/api';
+axios.defaults.timeout=8000;
+
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res=response.data;
+  if(res.status==0){
+    return res.data;
+  }else if(res.status==10){
+    window.location.href='/#/login';
+  }else{
+    alert(res.msg);//ElementUI的主键方式message弹窗错误消息
+  }
+});
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({
