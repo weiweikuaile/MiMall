bba554c1b6d8d0471e71fb2958520c1148cd1a17
weili <weiwork0012010@sina.com>
2020-07-18 22:52:31 +0800

    顶部导航条样式

--- src/App.vue
+++ src/App.vue
@@ -14,38 +14,15 @@ export default {
   },
   data(){
     return {
-      res:{}
+      
     }
   },
   mounted(){
-    //storage.setItem('a',1);
-    //storage.setItem('user',{'a':1});//或storage.setItem('user',{a:1});
-    //storage.setItem('abc',{a:1},'user');
-    // storage.clear('a');
-    //storage.clear('a','user');
-    //1、本地创建json.(本地加载请求静态json文件的形式)
-    // this.axios.get('/mock/user/login.json').then((res)=>{
-    //    this.res=res;
-    // });
-    // 2、easy-mock平台(通过easy-mock平台实现数据mock) 
-    // this.axios.get('/user/login').then((res)=>{
-    //    this.res=res;
-    // });
-    // 3、本地集成mockjs实现数据mock 
-    this.axios.get('/user/login').then((res)=>{
-       this.res=res;
-    });
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss' 
 </style>
