42abf33659dfacf2d116a1db7dc2f177edd73133
weili <weiwork0012010@sina.com>
2020-07-21 17:10:51 +0800

    5-3Nav-Header组件（3）-顶部导航条menu search样式 mixin.scss

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -35,8 +35,11 @@
                     </div> 
                 </div>
                 <div class="header-search">
-                    <input type="text" name="" id="" value=""/>
-                    <button>搜索</button>
+                    <div class="wrapper">
+                      <input type="text" name="keyword"/>
+                      <a href="javascript:;"></a>
+                    </div>
+                    
                 </div>
             </div>
         </div>
@@ -50,6 +53,7 @@
 </script> 
 <style lang="scss">
 @import './../assets/scss/base.scss';
+@import './../assets/scss/mixin.scss';
     .header{
        .nav-topbar{
          height:39px;
@@ -58,9 +62,7 @@
          color:#B0B0B0;
          font-size:12px;
          .container{
-            display:flex;
-            justify-content:space-between;
-            align-items:center;
+            @include flex();
             a{
                 display:inline-block;
                 color:#B0B0B0;
@@ -72,12 +74,8 @@
                 text-align: center;
                 color:#ffffff;
                 .icon-cart{
-                    display:inline-block;
-                    background:url('/imgs/icon-cart-checked.png') no-repeat center;
-                    background-size:contain;
+                    @include bgImg(16px,12px,'/imgs/icon-cart-checked.png');                    
                     margin-right: 4px;
-                    width:16px;
-                    height:12px;
                 }
             }
             
@@ -87,9 +85,7 @@
        .nav-header{
          .container{
              height:112px;
-             display:flex;
-             justify-content:space-between;
-             align-items:center;
+             @include flex();
              .header-logo{
                  display:inline-block;
                  width:55px;
@@ -101,20 +97,12 @@
                      height:55px;
                      &:before{
                          content:' ';
-                         display:inline-block;
-                         width:55px;
-                         height:55px;
-                         background:url('/imgs/mi-logo.png') no-repeat center;
-                         background-size:55px; 
+                         @include bgImg(55px,55px,'/imgs/mi-logo.png',55px);
                          transition:margin .2s;
                      }
                      &:after{
                          content:' ';
-                         display:inline-block;
-                         width:55px;
-                         height:55px;
-                         background:url('/imgs/mi-home.png') no-repeat center;
-                         background-size:55px; 
+                         @include bgImg(55px,55px,'/imgs/mi-home.png',55px);
                      }
                      &:hover:before{
                          margin-left:-55px;
@@ -124,7 +112,7 @@
              }
              .header-menu{
                  display:inline-block;
-                 width:200px;
+                 width:643px;
                  padding-left:209px;
                  .item-menu{
                     display:inline-block;
@@ -132,11 +120,36 @@
                     font-weight:bold;
                     font-size:16px;
                     line-height:112px;
-                    // margin-left:20px;
+                    margin-left:20px;
                     span{
                       cursor:pointer;
                     }
+                    &:hover{
+                        color:#333333;
+                    }
                  }
+             }
+             .header-search{
+                 width:319px;
+                .wrapper{
+                    height:50px;
+                    border:1px solid #e0e0e0;
+                    display:flex;
+                    align-items: center;
+                    input{
+                        border:none;
+                        border-right:1px solid #e0e0e0;
+                        width:264px;
+                        height:50px;
+                        padding-left:14px;
+                        box-sizing:border-box;
+                    }
+                    a{
+                        @include bgImg(18px,18px,'/imgs/icon-search.png');
+                        margin-left:17px;
+                    }
+
+                } 
              }
          }
        }
