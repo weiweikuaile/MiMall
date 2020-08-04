d3cd5793c980bd2ae92e6d5c206fd13718f4e3ce
weili <weiwork0012010@sina.com>
2020-08-04 20:33:00 +0800

    5-8首页轮播功能实现swiper:自动切换轮播速度,鼠标控制切换,阴影效果,前进后退按钮

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,10 +2,11 @@
     <div class="index">
         <div class="container">
             <div class="swiper-box">
-             <swiper :options="swiperOptions">
+             <swiper :options="swiperOptions" ref="mySwiper" v-if="slideList.length>1">
                  <swiper-slide v-for="(item,index) in slideList" :key="index">
                     <a :href="'/#/product/'+item.id"><img :src="item.img"></a>   
                  </swiper-slide>
+                 <!-- optional controls -->
                  <div class="swiper-pagination" slot="pagination"></div>
                  <div class="swiper-button-prev" slot="button-prev"></div>
                  <div class="swiper-button-next" slot="button-next"></div>
@@ -20,23 +21,38 @@
 </template> 
 <script>
 
-import ServiceBar from './../components/ServiceBar'
+import ServiceBar from './../components/ServiceBar';
 //import { swiper, swiperSlide} from 'vue-awesome-swiper'
-import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+//import { Swiper, SwiperSlide} from 'vue-awesome-swiper';
 //import 'swiper/css/swiper.css'//
-import 'swiper/swiper-bundle.css'
+import 'swiper/dist/css/swiper.css';
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+//import 'swiper/swiper-bundle.css';
+// var mySwiper=new Swiper('.swiper-container',{
+//         pagination:{
+//             el:'.swiper-pagination',
+//             //clickable:true,
+//             type: 'fraction',
+//         },
+// });
 export default{
     name:'index',
     components:{
-      Swiper,
-      SwiperSlide,  
+      swiper,
+      swiperSlide,  
       ServiceBar
     },
-    data(){
+    
+    data() {
         return {
            swiperOptions:{
+               loop : true,
                autoplay:true,
-               loop:true,
+               speed:1000,
+               pagination: {
+                 el: '.swiper-pagination',
+                 clickable:true,
+                },
                effect:'cube',
                cubeEffect: {
                  slideShadows: true,
@@ -44,15 +60,11 @@ export default{
                  shadowOffset: 100,
                  shadowScale: 0.6
                 },
-                pagination: {
-                 el: '.swiper-pagination',
-                 clickable:true
-                },
                 navigation: {
                  nextEl: '.swiper-button-next',
                  prevEl: '.swiper-button-prev',
                 },
-            },
+            },//swiperOptions
            slideList:[
                {
                    id:'42',
@@ -75,9 +87,23 @@ export default{
                    img:'/imgs/slider/slide-5.jpg'
                }
            ] 
+        }//return
+    },//data
+  
+    computed:{
+        swiper(){
+            //return this.$refs.mySwiper.swiper;//v3.1.3vue-awesome-swiper//报错
+            //return '2';
+            //return this.$refs.mySwiper.swiperInstance;
+            //return this.$refs.mySwiper.$swiper;//v4.1.1vue-awesome-swiper
         }
+    },
+    mounted(){
+        //console.log('this is current swiper instance object',this.swiper);
+        //this.swiper.slideTo(2, 1000, true);//跳到指定图片开始轮播
     }
-}
+}//export
+
 </script>
 <style lang="scss">
 .index{
@@ -88,8 +114,11 @@ export default{
                 width:100%;
                 height:100%;
             }
+           .swiper-pagination-bullet-active{
+               background: #fff;
+           }
+  
         }  
     }
 }
-
 </style>    
