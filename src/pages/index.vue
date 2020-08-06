520dda87d7a92e62240a72da7d40c710642b1985
weili <weiwork0012010@sina.com>
2020-08-06 23:25:34 +0800

    5-11_首页广告位实现ads-box和banner

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -59,8 +59,17 @@
                  <div class="swiper-button-next" slot="button-next"></div>
              </swiper>
             </div>
-            <div class="ads-box"></div>
-            <div class="banner"></div>
+            <div class="ads-box">
+                <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+                    <img :src="item.img" alt="">
+                </a>
+                 
+            </div>
+            <div class="banner">
+                <a href="/#/product/38" >
+                    <img src="/imgs/banner-1.png" alt="">
+                </a>
+            </div>
             <div class="product-box"></div>
         </div>
         <Service-bar></Service-bar>
@@ -153,6 +162,22 @@ export default{
                    img:'/imgs/item-box-4.jpg'
                }],
                [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+           ],
+           adsList:[
+               {
+                   id:33,
+                   img:'/imgs/ads/ads-1.png',
+               },
+               {
+                   id:48,
+                   img:'/imgs/ads/ads-2.jpg',
+               },{
+                   id:45,
+                   img:'/imgs/ads/ads-3.png',
+               },{
+                   id:47,
+                   img:'/imgs/ads/ads-4.jpg',
+               }
            ] 
         }//return
     },//data
@@ -263,6 +288,20 @@ export default{
            }
   
         }  
+    }
+    .ads-box{
+        @include flex();
+        margin-top:14px;
+        margin-bottom:31px;
+        a{
+            
+                width:296px;
+                height:167px;
+            
+        }//a
+    }//ads-box
+    .banner{
+        margin-bottom:50px;
     }
 }
 </style>    
