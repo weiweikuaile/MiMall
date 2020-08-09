27fc56a4129e628e9d0120b034922480f569d7f4
weili <weiwork0012010@sina.com>
2020-08-09 16:33:43 +0800

    5-12_手机商品实现（1）手机列表(静态) 展示及样式，本地data写2维数组，2次循环。

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -62,18 +62,41 @@
             <div class="ads-box">
                 <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
                     <img :src="item.img" alt="">
-                </a>
-                 
+                </a> 
             </div>
             <div class="banner">
                 <a href="/#/product/38" >
                     <img src="/imgs/banner-1.png" alt="">
                 </a>
             </div>
-            <div class="product-box"></div>
-        </div>
+        </div><!--index的container  -->
+        <div class="product-box">
+            <div class="container">
+                <h2>手机</h2>
+                <div class="wrapper">
+                    <div class="banner-left">
+                        <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+                    </div>
+                    <div class="list-box">
+                        <div class="list" v-for="(item,index) in phoneList" :key="index">
+                            <div class="item" v-for="(sub,j) in item" :key="j">
+                                <span>新品</span>
+                                <div class="item-img">
+                                    <img src="/imgs/item-box-3.jpg" alt="">
+                                </div>
+                                <div class="item-info">
+                                    <h3>小米9</h3>
+                                    <p>骁龙855,索尼4800万超广角微距</p>
+                                    <p class="price">2999元</p>
+                                </div>
+                            </div> 
+                        </div>       
+                    </div>
+                </div><!--wrapper-->
+            </div> <!--product-box的container  -->
+        </div><!--product-box  -->
         <Service-bar></Service-bar>
-    </div>
+    </div><!--index  -->
 </template> 
 <script>
 
@@ -178,10 +201,29 @@ export default{
                    id:47,
                    img:'/imgs/ads/ads-4.jpg',
                }
+           ],
+           phoneList:[
+               [1,1,1,1],[1,1,1,1]
            ] 
         }//return
     },//data
-  
+    mounted(){
+        //console.log('this is current swiper instance object',this.swiper);
+        //this.swiper.slideTo(2, 1000, true);//跳到指定图片开始轮播
+        //this.init();//初始化商品 加载商品列表
+    },
+    methods:{
+        // init(){
+        //    this.axios.get('/products',{
+        //        params:{
+        //            categoryId:100012,
+        //            pageSize:8
+        //        }
+        //    }).then((res)=>{
+
+        //    })
+        // }
+    },
     computed:{
         swiper(){
             //return this.$refs.mySwiper.swiper;//v3.1.3vue-awesome-swiper//报错
@@ -190,10 +232,7 @@ export default{
             //return this.$refs.mySwiper.$swiper;//v4.1.1vue-awesome-swiper
         }
     },
-    mounted(){
-        //console.log('this is current swiper instance object',this.swiper);
-        //this.swiper.slideTo(2, 1000, true);//跳到指定图片开始轮播
-    }
+    
 }//export
 
 </script>
@@ -303,5 +342,92 @@ export default{
     .banner{
         margin-bottom:50px;
     }
-}
+    .product-box{
+        background-color:$colorJ;
+        padding:30px 0 50px;
+        h2{
+            font-size:$fontF;
+            height:21px;
+            line-height:21px;
+            color:$colorB;
+            margin-bottom:20px;
+        }
+        .wrapper{
+            display: flex;
+            .banner-left{
+                margin-right:16px;
+                img{
+                    width: 224px;
+                    height: 619px;
+                }
+            }//banner-left
+            .list-box{
+                .list{
+                    // display: flex;//用mixin.scss里的flex()方法代替
+                    @include flex();
+                    width:986px;
+                    margin-bottom:14px;
+                    &:last-child{
+                        margin-bottom:0;
+                    }
+                    .item{
+                        width:236px;
+                        height:302px;
+                        background-color:$colorG;
+                        text-align:center;
+                        span{
+                            display: inline-block;
+                            font-size:$fontJ;
+                            line-height:24px;
+                            width:67px;
+                            height:24px;
+                            color:$colorG;
+                            &.new-pro{
+                                background-color:#7ECF68;
+                            }
+                            &.kill-pro{
+                                background-color:#E82626;
+                            }
+                        }//span
+                        .item-img{
+                            img{
+                                height:195px;
+                            }
+                        }//item-img
+                        .item-info{
+                            h3{
+                              font-size:$fontJ;
+                              line-height:$fontJ;
+                              color:$colorB;
+                              font-weight:bold;
+                            }
+                            p{
+                            //   font-size:$fontK;//系统默认字体大小是12px所以可以省略不写。
+                              line-height:13px;
+                              color:$colorD;
+                              margin:6px auto 13px;
+                            }//p
+                            .price{
+                                  color:#F20A0A;
+                                  font-size:$fontJ;
+                                  //line-height:$fontJ;//
+                                  //width:50px;//
+                                  //height:13px;//
+                                  font-weight:bold;
+                                  cursor:pointer;
+                                  &:after{
+                                    @include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
+                                    content:' ';
+                                    margin-left:5px;
+                                    vertical-align:middle;
+
+                                  }
+                            }//price
+                        }//item-info
+                    }//item
+                }//list
+            }//list-box
+        }//wrapper
+    }//product-box
+}//index
 </style>    
