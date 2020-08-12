b9b609a72869f3629ef34ed1fdbf3930987ca801
weili <weiwork0012010@sina.com>
2020-08-12 22:26:25 +0800

    5-17_Modal交互补充:依据父组件中参数的名称(可自定义名称)和对应的值的设定，子组件标题和按钮文本内容可设置成参数里的名称，方便组件扩展.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,18 +6,18 @@
             </div>
             <div class="modal-dialog">
                 <div class="modal-header">
-                    <span>标题</span>
+                    <span>{{title}}</span>
                     <a href="javascript:;" class="icon-close" v-on:click="$emit('cancel')"></a>
                 </div>
                 <div class="modal-body">
                     <slot name="body"></slot>
                 </div>
                 <div class="modal-footer">
-                    <a href="javascript:;" class="btn" v-if="btnType==1" v-on:click="$emit('submit')">确定</a>
-                    <a href="javascript:;" class="btn" v-if="btnType==2" v-on:click="$emit('cancel')">取消</a>
+                    <a href="javascript:;" class="btn" v-if="btnType==1" v-on:click="$emit('submit')">{{sureText}}</a>
+                    <a href="javascript:;" class="btn" v-if="btnType==2" v-on:click="$emit('cancel')">{{cancelText}}</a>
                     <div class="btn-group" v-if="btnType==3" v-else>
-                        <a href="javascript:;" v-on:click="$emit('submit')" class="btn">确定</a>
-                        <a href="javascript:;" v-on:click="$emit('cancel')" class="btn">取消</a>
+                        <a href="javascript:;" v-on:click="$emit('submit')" class="btn">{{sureText}}</a>
+                        <a href="javascript:;" v-on:click="$emit('cancel')" class="btn">{{cancelText}}</a>
                     </div>                
                 </div>
             </div>
