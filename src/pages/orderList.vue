15e6d450e32db6c1219438e9880c58416aebe063
weili <weiwork0012010@sina.com>
2020-12-16 13:26:10 +0800

    14-6_订单列表分页-滚动加载:第三种分页方式,即瀑布流,插件vue-infinite-scroll图片svg定时器setTimeout累加concat下一页hasNextPage最后一页第二种分页方式"加载更多"按钮消失

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -56,8 +56,15 @@
               @current-change="handleChange"
               >
             </el-pagination>
-            <div class="load-more">
+            <div class="load-more" v-if="showNextPage">
                 <el-button type="primary" :loading="loading" @click="loadMore">加载更多</el-button>
+            </div>
+            <div class="scroll-more" 
+              v-infinite-scroll="scrollMore" 
+              infinite-scroll-disabled="busy" 
+              infinite-scroll-distance="440"
+            >
+              <img src="/imgs/loading-svg/loading-spinning-bubbles.svg" alt="" v-show="loading">
             </div>  
             <no-data v-if="!loading && list.length==0"></no-data> 
           </div><!--order-box-->
@@ -70,6 +77,7 @@ import OrderHeader from './../components/OrderHeader';
 import Loading from './../components/Loading';
 import NoData from './../components/NoData';
 import { Pagination,Button } from 'element-ui';
+import infiniteScroll from 'vue-infinite-scroll';
     export default{
         name:'order-list',
         components:{
@@ -79,13 +87,18 @@ import { Pagination,Button } from 'element-ui';
           [Pagination.name]:Pagination,
           [Button.name]:Button
         },
+        directives: {
+          infiniteScroll
+        },
         data(){
           return {
             loading:false,//数据还未返回来时,显示loading
             list:[],//订单列表 接口2获取到的数据
             pageSize:10,//每页10条记录
             pageNum:1,//当前页 默认第1页
-            total:0//总条目数
+            total:0,//总条目数
+            showNextPage:true,//默认true显示 加载更多，是否显示按钮
+            busy:false,//默认false开启 滚动条滚动触发"滚动加载" true是禁用滚动条滚动触发"滚动加载"
           }
         },
         mounted(){
@@ -94,6 +107,7 @@ import { Pagination,Button } from 'element-ui';
         methods:{
           getOrderList(){//获取订单列表 接口2.订单List GET /orders
             this.loading=true;//当点击的时候设置为true即 显示loading
+            this.busy=true;//关闭滚动加载
             this.axios.get('/orders').then((res)=>{
           /*接口能用时再用这个params传参,现在mock无法模拟params带参,暂时用原来的不带参数的*/
           //   this.axios.get('/orders',{
@@ -102,12 +116,15 @@ import { Pagination,Button } from 'element-ui';
           //       pageNum:this.pageNum
           //     }
           //  }).then((res)=>{  
-              this.loading=false;
+              this.loading=false;//接口请求回来后,关闭loading
+              this.busy=false;//开启滚动加载
               //console.log(res.list)
-              //this.list=res.list;
+              
               this.list=this.list.concat(res.list);//把第二页数据追加到第一页数据后边,使用拼接数组
+              //this.list=res.list;
               //this.list=[]||res.list;//当没有数据时为空数组 测试NoData组件,测试结束后还原上一行代码
               this.total=res.total;
+              this.showNextPage=res.hasNextPage;//有下一页就显示"加载更多"按钮；最后一页不显示"加载更多"按钮。
             }).catch(()=>{
               this.loading=false;
             })
@@ -130,14 +147,50 @@ import { Pagination,Button } from 'element-ui';
               }
             })
           },
+          //第一种分页方式:element官网Pagination分页
           handleChange(pageNum){
             this.pageNum=pageNum;
             this.getOrderList();
           },
+          // 第二种分页方式:加载更多
           loadMore(){
             this.pageNum++;
             this.getOrderList();
-          }
+          },
+          //第三种分页方式:滚动加载 通过npm插件实现
+          scrollMore(){
+            this.busy=true;//默认false开启滚动条滚动触发"滚动加载";true是禁用滚动条滚动触发"滚动加载"
+            setTimeout(() => {
+              this.pageNum++;
+              this.getList();//调试滚动加载时,需要注销这行代码
+            }, 500);                          
+          },
+          //单独定义 专门给scrollMore使用 (类似getOrderList())
+          getList(){//获取订单列表 接口2.订单List GET /orders
+            this.loading=true;//当点击的时候设置为true即 显示loading
+            this.axios.get('/orders').then((res)=>{
+          /*接口能用时再用这个params传参,现在mock无法模拟params带参,暂时用原来的不带参数的*/
+          //   this.axios.get('/orders',{
+          //     params:{
+          //       pageSize:10,//每页10条记录
+          //       pageNum:this.pageNum
+          //     }
+          //  }).then((res)=>{ 
+              this.loading=false;//请求回来时,关闭loading 
+              //console.log(res.list) 
+              this.list=this.list.concat(res.list);//把第二页数据追加到第一页数据后边,使用拼接数组
+              //this.list=res.list;
+              //this.list=[]||res.list;//当没有数据时为空数组 测试NoData组件,测试结束后还原上一行代码
+              //如果有下一页 开启滚动加载;没有下一页 禁用滚动加载.
+              if(res.hasNextPage){
+                this.busy=false;//开启
+              }else{
+                this.busy=true;//禁用
+
+              }
+              
+            });
+          },
         }
     }
 </script>
@@ -215,7 +268,7 @@ import { Pagination,Button } from 'element-ui';
           background-color: #FF6600;
           border-color: #FF6600;
         }
-        .load-more{
+        .load-more,.scroll-more{
           text-align:center;
         }
       }
