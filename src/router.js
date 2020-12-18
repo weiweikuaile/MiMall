2dd3aaf2197dc4757e4145937fad7da33c51c86a
weili <weiwork0012010@sina.com>
2020-12-19 03:28:24 +0800

    15-4_路由按需加载方案(2)：promise,箭头函数,加载import,需安装syntax-dynamic-import插件,login,order,orderList这三个的路由用的import,路由分割表现,图片性能优化3种,预加载prefetch

--- src/router.js
+++ src/router.js
@@ -6,9 +6,9 @@ import Index from './pages/index'
 // import Product from './pages/product'
 // import Detail from './pages/detail'
 import Cart from './pages/cart'
-import Order from './pages/order'
+// import Order from './pages/order'
 import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
+// import OrderList from './pages/orderList'
 import OrderPay from './pages/orderPay'
 import AliPay from './pages/alipay'
 Vue.use(Router);
@@ -38,7 +38,7 @@ export default new Router({
       {
         path:'/login',
         name:'login',
-        component:resolve=>require(['./pages/login.vue'],resolve),  
+        component:()=>import('./pages/login.vue'),  
       },
       {
         path:'/cart',
@@ -48,12 +48,12 @@ export default new Router({
       {
         path:'/order',
         name:'order',
-        component:Order, 
+        component:()=>import('./pages/order.vue'), 
         children:[
             {
              path:'list',
              name:'order-list',
-             component:OrderList,
+             component:()=>import('./pages/orderList.vue'),
             },
             {
                 path:'confirm',
