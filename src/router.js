1b9d1a9897c1f4bb8096615b6195a6b0f499dec5
weili <weiwork0012010@sina.com>
2020-12-18 22:40:01 +0800

    15-3_路由按需加载方案(1)：promise,箭头函数,加载require,抛出resolve不需额外安装插件

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/home'
-import Login from './pages/login'
+// import Login from './pages/login'
 import Index from './pages/index'
-import Product from './pages/product'
-import Detail from './pages/detail'
+// import Product from './pages/product'
+// import Detail from './pages/detail'
 import Cart from './pages/cart'
 import Order from './pages/order'
 import OrderConfirm from './pages/orderConfirm'
@@ -27,18 +27,18 @@ export default new Router({
              },{
                 path:'/product/:id',
                 name:'product',
-                component:Product, 
+                component:resolve=>require(['./pages/product.vue'],resolve), 
               },{
                 path:'/detail/:id',
                 name:'detail',
-                component:Detail,  
+                component:resolve=>require(['./pages/detail.vue'],resolve),  
               }
          ]
       },
       {
         path:'/login',
         name:'login',
-        component:Login,  
+        component:resolve=>require(['./pages/login.vue'],resolve),  
       },
       {
         path:'/cart',
